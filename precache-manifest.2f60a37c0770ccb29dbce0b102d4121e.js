424754a185695c531fbac563fbbf14633da84833
Denis Kochetov <57500456+DenisKochetov@users.noreply.github.com>
2020-10-21 13:22:31 +0500

    Updates

---
+++ precache-manifest.2f60a37c0770ccb29dbce0b102d4121e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af32ee16a14b29449b6ed39c42676614",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "2845627e2ba9f3c382a4",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0fa516d394ff940c44a",
+    "url": "/react/static/js/2.55a84463.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react/static/js/2.55a84463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2845627e2ba9f3c382a4",
+    "url": "/react/static/js/main.9b7dc5e9.chunk.js"
+  },
+  {
+    "revision": "303020cb53d41a25c9b6",
+    "url": "/react/static/js/runtime-main.ab29a4de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
